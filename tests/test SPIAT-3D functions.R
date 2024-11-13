f09999a6906ccef5de5864d2c0067e84909f7714
David Le <dle@nid002542.hostmgmt.cm.setonix.pawsey.org.au>
2024-11-13 08:18:20 +0800

    New script to test each SPIAT-3D function against simulations to check if they work and when they fail.

---
+++ tests/test SPIAT-3D functions.R
@@ -0,0 +1,12 @@
+# Cases to test:
+# Correct input
+# Not spe object input
+# spe object input, but with invalid feature colname
+# 0 cells
+# 1 cell
+# 2 cells
+# Many cells, but 0 cell type of interest/reference/target
+# Many cells, but 1 cell type of interest/reference/target
+# Many cells, but 2 cell type of interest/reference/target
+# One cell type only
+#
