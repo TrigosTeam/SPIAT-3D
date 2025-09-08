18c804476c566080cef6ca0f555767370b4307e8
David Le <dle@nid001640.hostmgmt.cm.setonix.pawsey.org.au>
2025-09-08 16:44:47 +0800

    Updated.

--- SPIAT-3D data frame all functions.R
+++ SPIAT-3D data frame all functions.R
@@ -1221,8 +1221,8 @@ calculate_cross_L_gradient3D <- function(spatial_df,
   result$radius <- radii
   
   if (plot_image) {
-    fig1 <- plot_cross_gradient3D(result, 'L')
-    fig2 <- plot_cross_gradient_ratio3D(result, 'L')
+    fig1 <- plot_cross_L_gradient3D(result)
+    fig2 <- plot_cross_L_gradient_ratio3D(result)
     
     combined_fig <- plot_grid(fig1, fig2, nrow = 2)
     methods::show(combined_fig)
@@ -3368,40 +3368,4 @@ summarise_distances_between_cell_types3D <- function(distances_df) {
 }
 
 
-calculate_cross_L_gradient3D <- function(spatial_df, 
-                                         reference_cell_type, 
-                                         target_cell_types, 
-                                         radii, 
-                                         feature_colname = "Cell.Type",
-                                         plot_image = TRUE) {
-  
-  if (!(is.numeric(radii) && length(radii) > 1)) {
-    stop("`radii` is not a numeric vector with at least 2 values")
-  }
-  
-  result <- data.frame(matrix(nrow = length(radii), ncol = 2 + length(target_cell_types)))
-  colnames(result) <- c("reference", "expected", target_cell_types)
-  
-  for (i in seq(length(radii))) {
-    cross_L_df <- calculate_cross_L3D(spatial_df,
-                                      reference_cell_type,
-                                      target_cell_types,
-                                      radii[i],
-                                      feature_colname)
-    
-    result[i, ] <- cross_L_df
-  }
-  
-  # Add a radius column to the result
-  result$radius <- radii
-  
-  if (plot_image) {
-    fig1 <- plot_cross_L_gradient3D(result)
-    fig2 <- plot_cross_L_gradient_ratio3D(result)
-    
-    combined_fig <- plot_grid(fig1, fig2, nrow = 2)
-    methods::show(combined_fig)
-  }
-  
-  return(result)
-}
+
