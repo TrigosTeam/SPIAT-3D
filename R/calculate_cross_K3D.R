6df15585e3f41ac011cfeeccddd77be9e664cb9d
User <User@MacBook-Pro-7.local>
2026-02-11 20:26:06 +1100

    Added comments to explain cross K, cross K gradient, cross L and cross L gradient functions.

--- R/calculate_cross_K3D.R
+++ R/calculate_cross_K3D.R
@@ -1,9 +1,41 @@
+#' @title Calculate cross K-function on 3D spatial data.
+#'
+#' @description This function calculates the cross K-function on a 3D 
+#'     SpatialExperiment Object. See paper on theory behind cross K-function (I
+#'     ain't explaining it here).
+#' 
+#' @param spe A SpatialExperiment object containing 3D spatial information for 
+#'     the cells. Naming of spatial coordinates MUST be "Cell.X.Position", 
+#'     "Cell.Y.Position", "Cell.Z.Position" for the x-coordinate, y-coordinate 
+#'     and z-coordinate of each cell.
+#' @param reference_cell_type A string specifying the reference cell type.
+#' @param target_cell_types A character vector specifying the target cell types.
+#' @param radius A positive numeric specifying the radius value.
+#' @param feature_colname A string specifying the name of the column in the 
+#'     `colData` slot of the SpatialExperiment object that contains the cell 
+#'     type information. Defaults to "Cell.Type".
+#'
+#' @return A data frame containing observed cross K-function values for each
+#'     target cell type and expected cross K-function values.
+#'
+#' @examples
+#' result <- calculate_cross_K3D(
+#'     spe = SPIAT-3D::simulated_spe,
+#'     reference_cell_type = "Tumour",
+#'     target_cell_types = c("Tumour", "Immune"),
+#'     radius = 30
+#'     feature_colname = "Cell.Type"
+#' )
+#' 
+#' @export
+
 calculate_cross_K3D <- function(spe, 
                                 reference_cell_type, 
                                 target_cell_types, 
                                 radius, 
                                 feature_colname = "Cell.Type") {
   
+  # Check if inputs are valid
   if (is.null(spe[[feature_colname]])) stop(paste("No column called", feature_colname, "found in spe object"))
   
   if (is.null(spe[["Cell.ID"]])) {
@@ -30,6 +62,7 @@ calculate_cross_K3D <- function(spe,
   length <- round(max(spe_coords$Cell.X.Position) - min(spe_coords$Cell.X.Position))
   width  <- round(max(spe_coords$Cell.Y.Position) - min(spe_coords$Cell.Y.Position))
   height <- round(max(spe_coords$Cell.Z.Position) - min(spe_coords$Cell.Z.Position))
+  
   ## Get volume of the window the cells are in
   volume <- length * width * height
   
@@ -47,6 +80,7 @@ calculate_cross_K3D <- function(spe,
                                                               show_summary = FALSE,
                                                               plot_image = FALSE)
   
+  # Calculate cross K-fucnction for each target cell type
   for (target_cell_type in target_cell_types) {
     
     n_ref_tar_interactions <- sum(neighbourhood_counts_df[[target_cell_type]])
--- R/calculate_cross_L_gradient3D.R
+++ R/calculate_cross_L_gradient3D.R
@@ -1,3 +1,39 @@
+#' @title Calculate cross L gradient on 3D spatial data.
+#'
+#' @description This function calculates the cross L gradient on a 3D 
+#'     SpatialExperiment Object, which is the linearised transformation of the 
+#'     cross K gradient.
+#' 
+#' @param spe A SpatialExperiment object containing 3D spatial information for 
+#'     the cells. Naming of spatial coordinates MUST be "Cell.X.Position", 
+#'     "Cell.Y.Position", "Cell.Z.Position" for the x-coordinate, y-coordinate 
+#'     and z-coordinate of each cell.
+#' @param reference_cell_type A string specifying the reference cell type.
+#' @param target_cell_types A character vector specifying the target cell types.
+#' @param radii A positive, ascending numeric vector specifying the set of 
+#'     radius values used.
+#' @param feature_colname A string specifying the name of the column in the 
+#'     `colData` slot of the SpatialExperiment object that contains the cell 
+#'     type information. Defaults to "Cell.Type".
+#' @param plot_image A logical indicating whether to plot cross L gradient as a 
+#'     line graph showing both the observed cross L values for each cell type 
+#'     and expected cross L values. Defaults to TRUE.
+#'
+#' @return A data frame containing observed cross L values for each target cell 
+#'     type and expected cross L values (columns), all across each radii (rows).
+#'
+#' @examples
+#' result <- calculate_cross_L_gradient3D(
+#'     spe = SPIAT-3D::simulated_spe,
+#'     reference_cell_type = "Tumour",
+#'     target_cell_types = c("Tumour", "Immune"),
+#'     radii = seq(20, 100, 10),
+#'     feature_colname = "Cell.Type",
+#'     plot_image = TRUE
+#' )
+#' 
+#' @export
+
 calculate_cross_L_gradient3D <- function(spe, 
                                          reference_cell_type, 
                                          target_cell_types, 
