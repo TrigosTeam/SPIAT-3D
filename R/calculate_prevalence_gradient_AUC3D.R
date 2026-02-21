c1fdec0f4bf7ebbbbfcca7a0a8d4c9c3926f4f8c
User <User@MacBook-Pro-7.local>
2026-02-21 13:20:04 +1100

    Removed separate function and embedded into calculate prevalence gradient function.

--- R/calculate_prevalence_gradient_AUC3D.R
+++
@@ -1,7 +0,0 @@
-calculate_prevalence_gradient_AUC3D <- function(prevalence_gradient_df) {
-  
-  return(
-    sum(diff(prevalence_gradient_df$threshold) * (head(prevalence_gradient_df$prevalence, -1) + tail(prevalence_gradient_df$prevalence, -1)) / 2)
-  )
-  
-}
